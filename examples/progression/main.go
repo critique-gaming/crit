19cdb3bcf51878b0064daec8ee10f4e18aa2bdeb
Marius Petcu <dapetcu21@gmail.com>
2019-01-19 18:39:26 +0200

    Implement progression example

---
+++ examples/progression/main.go
@@ -0,0 +1,15 @@
+components {
+  id: "main"
+  component: "/examples/progression/main.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

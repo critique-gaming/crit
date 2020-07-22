2d63c9ab5c2e58b367a261bd8663d54d8a807b97
Marius Petcu <dapetcu21@gmail.com>
2020-07-22 03:16:47 +0300

    Fix documentation errors

--- crit/pick.lua
+++ crit/pick.lua
@@ -37,17 +37,17 @@ local no_padding = {
 ]]
 -- @param[type=url | string] sprite_url An URL identifying the sprite component.
 -- @number x The x position of the point (in world space) to do the check on.
--- @number x The y position of the point (in world space) to do the check on.
+-- @number y The y position of the point (in world space) to do the check on.
 -- @tparam[opt] PickPadding padding By how much should the hitbox of the sprite
 --   be expanded or constricted.
 -- @treturn boolean Returns `true` if the point hits inside the sprite and
 --   `false` otherwise.
-function M.pick_sprite(url, x, y, padding)
-  local transform = go.get_world_transform(url)
+function M.pick_sprite(sprite_url, x, y, padding)
+  local transform = go.get_world_transform(sprite_url)
   local pos = vmath.inv(transform) * vmath.vector4(x, y, 0, 1)
   x, y = pos.x, pos.y
 
-  local size = go.get(url, h_size)
+  local size = go.get(sprite_url, h_size)
   padding = padding or no_padding
 
   local half_width = size.x * 0.5
